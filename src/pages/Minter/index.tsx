69466d05754f202401891299b02baeafec7f70ec
Pankaj Pramanik <pkp2.me2k9@gmail.com>
2022-04-23 15:13:54 +0600

    done with  model and avatar minting

--- src/pages/Minter/index.tsx
+++ src/pages/Minter/index.tsx
@@ -6,25 +6,30 @@ import { contractABI, contractAddress } from "../../contract";
 import "./style.scss";
 import Web3Modal from "web3modal";
 import { ethers } from "ethers";
-import {Buffer} from 'buffer';
+import { Buffer } from "buffer";
 
 const Minter: React.FC = () => {
   const { authenticate, isAuthenticated, logout } = useMoralis();
   const [name, setName] = React.useState("");
   const [description, setDescription] = React.useState("");
   const [file, setFile] = React.useState(null);
+  const [previewFile, setPreviewFile] = React.useState(null);
   const onSubmit = async (e) => {
     e.preventDefault();
     try {
-      const file1:any = new Moralis.File(file.name, file);
+      const file1: any = new Moralis.File(file.name, file);
       await file1.saveIPFS();
       const file1url = file1.ipfs();
+      const previewImage: any = new Moralis.File(previewFile.name, file);
+      await previewImage.saveIPFS();
+      const previewImageurl = previewImage.ipfs();
       const metadata = {
         name,
         description,
-        image: file1url,
+        image: previewImageurl,
+        animation_url: file1url,
       };
-      const file2:any = new Moralis.File(`${name}metadata.json`, {
+      const file2: any = new Moralis.File(`${name}metadata.json`, {
         base64: Buffer.from(JSON.stringify(metadata)).toString("base64"),
       });
       await file2.saveIPFS();
@@ -33,7 +38,11 @@ const Minter: React.FC = () => {
       const connection = await web3Modal.connect();
       const provider = new ethers.providers.Web3Provider(connection);
       const signer = provider.getSigner();
-      const tokenContract = new ethers.Contract(contractAddress, contractABI, signer);
+      const tokenContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
       const transaction = await tokenContract.mint(metadataurl);
       const tx = await transaction.wait();
       const event = tx.events[0];
@@ -64,8 +73,17 @@ const Minter: React.FC = () => {
             placeholder="Description"
             onChange={(e) => setDescription(e.target.value)}
           />
-
-          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+          <div>
+            <label>Model file</label>
+            <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+          </div>
+          <div>
+            <label>Preview Image</label>
+            <input
+              type="file"
+              onChange={(e) => setPreviewFile(e.target.files[0])}
+            />
+          </div>
           <button type="submit">Mint now!</button>
           <button type="button" onClick={logout}>
             Logout
