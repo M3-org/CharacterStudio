cc321a9749fc528ae8e76050fc9b42b009910d27
Super <supernftier@gmail.com>
2022-05-10 20:03:30 +0300

    three

--- src/components/ConnectMint/index.tsx
+++ src/components/ConnectMint/index.tsx
@@ -367,7 +367,7 @@ export default function ConnectMint() {
               ) : (
                 <React.Fragment>
                   MINT Model <br /> Public
-                  Price: {mintPrice} ETH | {totalMinted}/{totalToBeMinted} Remaining
+                  Price: {mintPricePublic} ETH | {totalMinted}/{totalToBeMinted} Remaining
                 </React.Fragment>
               )}
             </Button>
