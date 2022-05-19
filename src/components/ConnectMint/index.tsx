92532bd76806ab681af613990df46d480193bb5a
Super <supernftier@gmail.com>
2022-05-19 15:57:07 +0300

    change mintedtoken

--- src/components/ConnectMint/index.tsx
+++ src/components/ConnectMint/index.tsx
@@ -55,6 +55,7 @@ export default function ConnectMint() {
     mintPrice,
     mintPricePublic,
     totalMinted,
+    setTotalMinted,
     gender,
     totalToBeMinted
   }: any = useGlobalState();
@@ -105,7 +106,7 @@ export default function ConnectMint() {
     setShowAlert(true);
     setTimeout(() => {
       setShowAlert(false);
-    }, 2000);
+    }, 4000);
   };
 
   const sendWhitelist = async () => {
@@ -182,7 +183,7 @@ export default function ConnectMint() {
     const responseUser = await axios.get(
       `${API_URL}/get-signature?address=${account}`
     );
-
+    console.log("response", responseUser);
     if (responseUser.data.signature) {
       let amountInEther = mintPrice;
       setIsPricePublic(1);
@@ -230,8 +231,14 @@ export default function ConnectMint() {
     return false;
   };
 
-  const handleOpenMintPopup = () => {
+  const handleOpenMintPopup = async () => {
     setMintPopup(true);
+
+    const signer = new ethers.providers.Web3Provider(ethereum).getSigner();
+    const contract = new ethers.Contract(contractAddress, contractABI, signer);
+    const MintedToken = await contract.totalSupply();
+    setTotalMinted(parseInt(MintedToken));
+
   };
   const handleCloseMintPopup = () => {
     setMintPopup(false);
