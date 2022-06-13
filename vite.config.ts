f7768b620975a19e6ae27d8ba88c1170e16416f1
shawticus <shawmakesmagic@gmail.com>
2022-06-13 09:12:31 -0700

    Prepare for lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite"
 import reactRefresh from "@vitejs/plugin-react-refresh"
 import path from "path"
+import dts from 'vite-plugin-dts';
+import react from '@vitejs/plugin-react';
+
 
 // See guide on how to configure Vite at:
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react(), reactRefresh(), dts({insertTypesEntry: true})],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'dist/src/index.js'),
