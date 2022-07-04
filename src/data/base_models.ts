b05cbb23d4d42afe809a5e6acbd8368506121036
DavinciDreams <64185677+DavinciDreams@users.noreply.github.com>
2022-07-04 08:27:22 -0400

    update avatar

--- src/data/base_models.ts
+++ src/data/base_models.ts
@@ -1,36 +1,22 @@
 export default [
   {
     "id": "1",
-    "name": "Male",
-    "file": "https://nolimitcrypto.s3.us-west-2.amazonaws.com/base_models_compressed/Male_Muscular.vrm",
-    "thumbnail": "https://nolimitcrypto.s3.us-west-2.amazonaws.com/base_models_compressed/Male_Muscular.png",
-    "format": "vrm",
-    "bodyTargets": [
-      "CC_Base_Body_1",
-      "CC_Base_Body_2",
-      "CC_Base_Body_3",
-      "CC_Base_Body_4",
-      "CC_Base_Body_5",
-      "CC_Base_Body_6"
-    ],
-    "traitsDirectory": "https://nolimitcrypto.s3.us-west-2.amazonaws.com/model_traits/male_muscular",
-    "thubnailsDirectory": "https://nolimitcrypto.s3.us-west-2.amazonaws.com/model_traits/thubnails"
+    "name": "Body1",
+    "file": "/body1.glb",
+    "animation": "/Animation.glb",
+    "thumbnail": "/body1.png",
+    "format": "glb",
+    "traitsDirectory": "https://github.com/Decentralized-Intelligence-Agency/interop-assets/tree/main/wearables/dcentralanded",    
+    "thumbnailsDirectory": "https://github.com/Decentralized-Intelligence-Agency/interop-assets/tree/main/wearables/dcentralanded",
   },
   {
     "id": "2",
-    "name": "Female",
-    "file": "https://nolimitcrypto.s3.us-west-2.amazonaws.com/base_models_compressed/Female_Thin.vrm",
-    "thumbnail": "https://nolimitcrypto.s3.us-west-2.amazonaws.com/base_models_compressed/Female_Thin.png",
-    "format": "vrm",
-    "bodyTargets": [
-      "CC_Base_Body_1",
-      "CC_Base_Body_2",
-      "CC_Base_Body_3",
-      "CC_Base_Body_4",
-      "CC_Base_Body_5",
-      "CC_Base_Body_6"
-    ],
-    "traitsDirectory": "https://nolimitcrypto.s3.us-west-2.amazonaws.com/model_traits/female_thin",
-    "thubnailsDirectory": "https://nolimitcrypto.s3.us-west-2.amazonaws.com/model_traits/thubnails"
+    "name": "body2",
+    "file": "/body2.glb",
+    "animation": "/Animation.glb",
+    "thumbnail": "/body1.png",
+    "format": "glb",
+    "traitsDirectory": "https://github.com/Decentralized-Intelligence-Agency/interop-assets/tree/main/wearables/dcentralanded",
+    "thumbnailsDirectory": "https://github.com/Decentralized-Intelligence-Agency/interop-assets/tree/main/wearables/dcentralanded"
   }
 ]
