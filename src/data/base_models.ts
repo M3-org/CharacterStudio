aa5fa4f1be805781dd3fd4757c51a4b31b492e39
intelliverse777 <intelliverse777@gmail.com>
2022-09-23 14:41:53 -0500

    update base model json

--- src/data/base_models.ts
+++ src/data/base_models.ts
@@ -1,30 +1,43 @@
 export default [
   {
     "id": "1",
-    "name": "Male",
-    "file": "./models/body1.glb",
+    "name": "Female",
+    "file": "./models/humanFemale.glb",
     "thumbnail": "/body.png",
     "format": "glb",
     "bodyTargets": [
       "Body",
+      "Mesh001",
+      "Mesh001_1",
+      "Mesh001_2",
+      "Mesh001_3",
+      "Mesh001_4",
+      "Mesh001_5",
     ],
-    "traitsDirectory": "https://webaverse.github.io/loot-assets/",
-    "thumbnailsDirectory": "https://webaverse.github.io/loot-assets/",
-    "traitsJsonPath": "https://webaverse.github.io/loot-assets/loot.json",
-    "animationPath": "./animations/Animation.glb"
+    "traitsDirectory": "https://memelotsqui.github.io/loot-assets/",
+    "thumbnailsDirectory": "https://memelotsqui.github.io/loot-assets/",
+    "traitsJsonPath": "https://memelotsqui.github.io/loot-assets/lootFemale.json",
+    "animationPath": "./animations/animationFemale.glb"
   },
   {
     "id": "2",
-    "name": "Female",
-    "file": "./models/body2.glb",
+    "name": "Male",
+    "file": "./models/HumanMale.glb",
     "thumbnail": "/body.png",
     "format": "glb",
     "bodyTargets": [
       "Body",
+      "Mesh",
+      "Mesh_1",
+      "Mesh_2",
+      "Mesh_3",
+      "Mesh_4",
+      "Mesh_5",
+      "Mesh_6",
     ],
-    "traitsDirectory": "https://webaverse.github.io/loot-assets/",
-    "thumbnailsDirectory": "https://webaverse.github.io/loot-assets/",
-    "traitsJsonPath": "https://webaverse.github.io/loot-assets/loot.json",
-    "animationPath": "./animations/Animation.glb"
+    "traitsDirectory": "https://memelotsqui.github.io/loot-assets/",
+    "thumbnailsDirectory": "https://memelotsqui.github.io/loot-assets/",
+    "traitsJsonPath": "https://memelotsqui.github.io/loot-assets/lootMale.json",
+    "animationPath": "./animations/animationMale.glb"
   }
 ]
