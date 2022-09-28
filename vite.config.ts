7eab86f8cfe980b3753b7245877e05a8484fe957
shawticus <shawmakesmagic@gmail.com>
2022-09-28 04:49:43 -0700

    remove dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite"
-import path from "path"
-import dts from 'vite-plugin-dts';
 import react from '@vitejs/plugin-react';
 
 
 // See guide on how to configure Vite at:
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({insertTypesEntry: true})]
+  plugins: [react()]
 })
