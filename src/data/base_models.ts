e1803cf30ccd34464c38c71ab1f73c2cea58b8ee
intelliverse777 <intelliverse777@gmail.com>
2022-10-21 20:46:47 -0500

    update base_model for loot model

--- src/data/base_models.ts
+++ src/data/base_models.ts
@@ -7,7 +7,11 @@ export default [
     "format": "vrm",
     "bodyTargets": [
       "Body_Female",
-      "Head_female"
+      "Head_femalebaked",
+      "Head_femalebaked_1",
+      "Head_femalebaked_2",
+      "Head_femalebaked_3",
+      "Head_femalebaked_4"
     ],
     "traitsDirectory": "https://memelotsqui.github.io/loot-assets/neurohacker/female/",
     "thumbnailsDirectory": "https://memelotsqui.github.io/loot-assets/neurohacker/female/",
@@ -22,12 +26,17 @@ export default [
     "format": "vrm",
     "bodyTargets": [
       "Body",
-      "Face_Male"
+      "Face_Malebaked",
+      "Face_Malebaked_1",
+      "Face_Malebaked_2"
     ],
-    
+
     "traitsDirectory": "https://memelotsqui.github.io/loot-assets/neurohacker/male/",
     "thumbnailsDirectory": "https://memelotsqui.github.io/loot-assets/neurohacker/male/",
     "traitsJsonPath": "https://memelotsqui.github.io/loot-assets/neurohacker/male/loot.json",
     "animationPath": "./animations/vrmanim.glb"
   }
 ]
+
+
+
