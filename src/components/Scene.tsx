bf1042b1303430943de8ae5720729e67ae80b1d8
intelliverse777 <intelliverse777@gmail.com>
2022-10-22 17:44:02 -0400

    add reflection effect

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -8,7 +8,8 @@ import Selector from "./Selector";
 import '../styles/scene.scss'
 import { position } from "html2canvas/dist/types/css/property-descriptors/position";
 import { sceneService } from "../services";
-import gsap from 'gsap';
+import { MeshReflectorMaterial } from '@react-three/drei/core/MeshReflectorMaterial'
+
 
 
 export default function Scene(props: any) {
@@ -59,7 +60,7 @@ export default function Scene(props: any) {
     const moveCamera = () => {
 
     }
-  
+
   return (
     <div style={{
       width: "100vw",
@@ -70,20 +71,20 @@ export default function Scene(props: any) {
         id="canvas-wrap"
         className={`canvas-wrap ${wrapClass && wrapClass}`}
         style={{ ...canvasWrap,
-            background : 'url(./mainBackground.png)',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover'
+            // background : 'url(./mainBackground.png)',
+            // backgroundPosition: 'center',
+            // backgroundRepeat: 'no-repeat',
+            // backgroundSize: 'cover'
           }}
       >
         <Canvas
           className="canvas"
           id="editor-scene"
         >
-          {/* <gridHelper
+           <gridHelper
             args={[50, 25, "#101010", "#101010"]}
             position={[0, 0, 0]}
-          /> */}
+          /> 
           <spotLight
             intensity={1}
             position={[0, 3.5, 2]}
@@ -110,29 +111,44 @@ export default function Scene(props: any) {
             shadow-mapSize-height={2048}
             castShadow
           />
-          {/* <OrbitControls
+          <directionalLight castShadow intensity={2} position={[10, 6, 6]} shadow-mapSize={[1024, 1024]}>
+            <orthographicCamera attach="shadow-camera" left={-20} right={20} top={20} bottom={-20} />
+          </directionalLight>
+           {/* <OrbitControls
             minDistance={1}
             maxDistance={3}
             minPolarAngle={0.0}
             maxPolarAngle={Math.PI / 2 - 0.1}
             enablePan={false}
             target={[0, 0.5, 0]}
-          /> */}
+          />  */}
           <PerspectiveCamera
-            
             aspect={1200 / 600}
             //radius={(1200 + 600) / 4}
             //fov={100}
-            position={[0.3, -0.9, 3.6]}
+            position={[0.3, -0.9, 1.6]}
             onUpdate={self => self.updateProjectionMatrix()}
-            
           >
-            {console.log(PerspectiveCamera)}
             {camera && <cameraHelper args={camera} />}
             {!downloadPopup && !mintPopup && (
               <TemplateModel scene={scene} />
             )}
+          <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+            <planeGeometry args={[3, 3]} />
+            <MeshReflectorMaterial
+              blur={[400, 400]}
+              resolution={2048}
+              mixBlur={0.8}
+              mixStrength={20}
+              depthScale={1}
+              minDepthThreshold={0.85}
+              color="#151515"
+              metalness={0.6}
+              roughness={1}
+            />
+          </mesh>
           </PerspectiveCamera>
+
         </Canvas>
       </div>
       <div style={{
