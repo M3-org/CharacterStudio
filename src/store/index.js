598d8a771b894b28e2a91ea126fde9cf6a1b708e
intelliverse777 <intelliverse777@gmail.com>
2022-11-08 21:56:26 -0500

    add store for loading state

--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,26 @@ const useMuteStore = create((set) => ({
     }))
 }))
 
+const useModelingStore = create((set) => ({
+    isModeling: new Array(3),
+    isComplete: new Array(3),
+    setModeling: (order, percent) => set((state) => {
+        let isModeling = [...state.isModeling];
+        isModeling[order] = percent;
+        return {
+            isModeling
+        }
+    }),
+    setComplete: (order, value) => set((state) => {
+        let isComplete = [...state.isComplete];
+        isComplete[order] = value;
+        return {
+            isComplete
+        }
+    })
+}))
+
 export {
-    useMuteStore
+    useMuteStore,
+    useModelingStore,
 };
