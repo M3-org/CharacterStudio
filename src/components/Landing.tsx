2e4922954f2c48a00607bc1b7b038532049caf7c
shawticus <shawmakesmagic@gmail.com>
2022-12-15 11:25:47 -0800

    make scene more 3d

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable no-inline-styles/no-inline-styles */
 
 import React, { useState, useEffect } from 'react';
 import { useSpring, animated } from 'react-spring'
@@ -6,11 +8,17 @@ import LoadingOverlayCircularStatic from './LoadingOverlay';
 import logo from '../ui/landing/logo.png'
 import passUrl from "../sound/class_pass.wav"
 import clickUrl from "../sound/class_click.wav"
-import ModelCanvas from './ModelCanvas';
 import { LandingPop } from './LandingPop';
 import { useMuteStore, useModelingStore, useDefaultTemplates, usePreModelClass } from '../store'
 import { StyledLanding } from '../styles/landing.styled.js'
 
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei/core/PerspectiveCamera";
+import { NoToneMapping, TextureLoader } from 'three';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import { VRM  } from "@pixiv/three-vrm"
+import { sceneService } from '../services/scene'
+import { AnimationManager } from '../library/animations/animationManager';
 
 export default function Landing(props){
 
@@ -22,6 +30,10 @@ export default function Landing(props){
     const defaultTemplates = useDefaultTemplates((state) => state.defaultTemplates)
     const setPreModelClass = usePreModelClass((state) => state.setPreModelClass)
 
+    const [controls, setControls] = useState<object>(Object);
+    const [drophunter, setDrophunter] = useState<object>(Object);
+    const [neurohacker, setNeurohacker] = useState<object>(Object);
+
     const [clicked, setClicked] = useState(false);
 
     const [isLoading, setIsLoading] = useState(false);
@@ -32,8 +44,20 @@ export default function Landing(props){
     const f_neuroHacker = "../3d/models/landing/f_neurohacker.vrm"
     const m_neuroHacker = "../3d/models/landing/neuro-noWeapon.vrm"
 
+    const [cameraFov, setCameraFov] = useState(20);
+    const [cameraPositionX, setCameraPositionX] = useState(0);
+    const [cameraPositionY, setCameraPositionY] = useState(-1.45);
+    const [cameraPositionZ, setCameraPositionZ] = useState(3.1);
+
     const anim_female = "../3d/animations/idle_webaverse.fbx";
     const anim_male = "../3d/animations/idle_male.fbx";
+
+    const [hovering, setHovering] = useState('');
+
+    // get ref camera
+
+    const camera = React.useRef();
+
     const [cardAnimation, setCardAnimation] = useSpring(() => ({
      from: { x: 0, opacity : 1 },
     }))
@@ -68,6 +92,39 @@ export default function Landing(props){
         setIsLoading(false);
         console.log("isloading")
     }
+
+
+    /////////////////////////////
+
+    useEffect(() => {
+        (async () => {
+            async function createModel(item){
+                const animManager = new AnimationManager();
+                const vrm = await sceneService.loadModel(item.model);
+                await animManager.loadAnimations(item.animation);
+                return {vrm, animManager}
+            }
+
+            await (async () => {
+                const {vrm, animManager} = await createModel(modelArr[0]);
+                animManager.startAnimation(vrm)
+                setDrophunter(vrm.scene)
+            })();
+
+            await (async () => {
+                const {vrm, animManager} = await createModel(modelArr[1]);
+                animManager.startAnimation(vrm)
+                setNeurohacker(vrm.scene)
+            })();
+
+
+            setIsLoading(false)
+            })()
+      }, [])
+
+      ////////////////////////////
+
+
     useEffect(() => {
         setIsLoading(true)
         // window.addEventListener("load", handleLoading);
@@ -142,8 +199,62 @@ export default function Landing(props){
                     />
                 )
             }
-            <animated.div style = {{...titleAnimation}}>
+
+                    <div className="drophunter" style={{
+                        position: "absolute",
+                        bottom: "40px",
+                        right: "0px",
+                        opacity: hovering === 'neurohacker' ? 1 : 0.5,
+                        zIndex: 10
+                    }}
+
+                    onMouseEnter={() => {
+                        setHovering('neurohacker')
+                    }}
+
+                    onMouseLeave={() => {
+                        if(hovering === 'neurohacker'){
+                            setHovering('');
+                        }
+                    }}
+
+                    // on mouse click
+                    onClick={() => {
+                        handleClick(1)
+                    }}
+                    >
+                        <img src={"public/DropHunter.svg"} />
+                    </div>
+
+                    <div className="neurohacker" style={{
+                        position: "absolute",
+                        bottom: "40px",
+                        left: "0px",
+                        opacity: hovering === 'drophunter' ? 1 : 0.5,
+                        zIndex: 10
+                    }}
+                    
+                    onMouseEnter={() => {
+                        setHovering('drophunter')
+                    }}
+
+                    onMouseLeave={() => {
+                        if(hovering === 'drophunter'){
+                            setHovering('');
+                        }
+                    }}
+
+                    // on mouse click
+                    onClick={() => {
+                        handleClick(2)
+                    }}
+                    >
+                        <img src={"public/NeuralHacker.svg"} />
+                    </div>
+
+        <animated.div style = {{...titleAnimation}}>
                     <div className="topBanner" >     
+
                         <img className ="webaverse-text" src={logo} />
                         <div className='studio' >Character Studio</div>
                     </div>
@@ -151,36 +262,52 @@ export default function Landing(props){
                         <div className='subTitle-text'>Pick a Class
                             <div className="subTitle-desc"> You'll be able to customize in a moment.</div>
                         </div>
+
                     </div>
-            </animated.div>
-            <animated.div 
-                className="imgs"
-                style={{
-                    ...cardAnimation,
+        </animated.div>
+
+            {/* simple floating div with sliders to control setCameraFov and setCameraPosition */}
+            <div style={{position: "absolute", top: 0, left: 0, zIndex: 1000}}>
+            </div>
+                <Canvas
+                style = {{
+                    // width: "calc(100%)",
+                    // position: "absolute",
+                    width : '100vw',
+                    height : '100vh',
+                    position: 'fixed',
                 }}
-            >
-                {
-                    modelArr.map((item, idx) => (
-                        <div className='characterGroup' key={idx}
-                            onMouseEnter={() => {
-                                setIsHovering(true);
-                                if(!isMute)
-                                    play();
-                            }}
-                            onMouseLeave={() => {
-                                setIsHovering(false);
-                                stop();
-                        }}
-                        onClick = {() => handleClick(item.index)}
-                        >
-                            <LandingPop className="landingPop" text={item.text} />
-                            <ModelCanvas 
-                                modelPath={item.model} 
-                                animation = {item.animation} 
-                                order = {item.index} 
-                            />
-                        </div>
-                    ))
-                }
-            </animated.div>
+                camera={{ fov: cameraFov }}
+                gl={{ antialias: true, toneMapping: NoToneMapping }}
+                //linear
+                //className="canvas"
+                >
+                <directionalLight 
+                    //castShadow = {true}
+                    intensity = {1} 
+                    color = {[0.6,.8,1]}
+                    position = {[-2, 3, 6]} 
+                    intensity={.2}
+                    shadow-mapSize = {[1024, 1024]}>
+                </directionalLight>
+                <ambientLight
+                    color={[0.6,0.9,1]}
+                intensity={0.5}
+                />
+                <PerspectiveCamera
+                    ref={camera}
+                    fov={20}
+                    position = {[cameraPositionX, cameraPositionY, cameraPositionZ]}
+                    rotation = {[-0,0,0]}
+                    onUpdate = {self => self.updateProjectionMatrix()}
+                >
+                <mesh position={[.4, 0, 0]} rotation={[0, -1, 0]}>
+                    <primitive object={drophunter} />
+                </mesh>
+                <mesh position={[-.4, 0, 0]} rotation={[0, 1, 0]}>
+                <primitive object={neurohacker} />
+                </mesh>
+
+                </PerspectiveCamera>
+            </Canvas>
         </StyledLanding>)}
