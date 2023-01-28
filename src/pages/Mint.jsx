10ffd0ddf86512f57bbca1c5c3ff63d294143596
Hayk <55106546+mavisakalyan@users.noreply.github.com>
2023-01-28 18:50:34 +0530

    Route updates. Popups.

--- src/pages/Mint.jsx
+++ src/pages/Mint.jsx
@@ -1,25 +1,50 @@
-import React from 'react';
-import styles from './Mint.module.css';
-import { ViewMode, ViewContext } from '../context/ViewContext';
+import React from "react"
+import styles from "./Mint.module.css"
+import { ViewMode, ViewContext } from "../context/ViewContext"
 
-import Mint from '../components/Mint';
+import Mint from "../components/Mint"
+import CustomButton from "../components/custom-button"
 
 function MintComponent() {
-    const { setViewMode } = React.useContext(ViewContext);
+  const { setViewMode } = React.useContext(ViewContext)
 
-    const back = () => {
-        console.log('back');
-        setViewMode(ViewMode.SAVE)
-    }
+  const back = () => {
+    console.log("back")
+    setViewMode(ViewMode.SAVE)
+  }
 
-    return (
-        <div className={styles.container}>
-            <Mint />
-            <div className={styles.buttonContainer}>
-                <button className={styles.button} onClick={back}>Back</button>
-            </div>
+  const next = () => {
+    console.log("next")
+    setViewMode(ViewMode.VIEW)
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.mintContainer}>
+        <div className={styles.topLine} />
+        <div className={styles.bottomLine} />
+        <div className={styles.scrollContainer}>
+          <Mint />
         </div>
-    );
+      </div>
+      <div className={styles.buttonContainer}>
+        <CustomButton
+          theme="light"
+          text="Back"
+          size={14}
+          className={styles.buttonLeft}
+          onClick={back}
+        />
+        <CustomButton
+          theme="light"
+          text="Chat"
+          size={14}
+          className={styles.buttonRight}
+          onClick={next}
+        />
+      </div>
+    </div>
+  )
 }
 
-export default MintComponent;
+export default MintComponent
--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import styles from './View.module.css';
-import { ViewMode, ViewContext } from '../context/ViewContext';
-import Chat from '../components/Chat';
+import React from "react"
+import styles from "./View.module.css"
+import { ViewMode, ViewContext } from "../context/ViewContext"
+import Chat from "../components/Chat"
+import CustomButton from "../components/custom-button"
 
 function ViewComponent() {
-    const { setViewMode } = React.useContext(ViewContext);
+  const { setViewMode } = React.useContext(ViewContext)
 
-    const back = () => {
-        console.log('back');
-        setViewMode(ViewMode.SAVE)
-    }
+  const back = () => {
+    console.log("back")
+    setViewMode(ViewMode.SAVE)
+  }
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.buttonContainer}>
-                <button className={styles.button} onClick={back}>Back</button>
-            </div>
-            <Chat />
+  return (
+    <div className={styles.container}>
+      <div className={styles.chatContainer}>
+        <div className={styles.topLine} />
+        <div className={styles.bottomLine} />
+        <div className={styles.scrollContainer}>
+          <Chat />
         </div>
-    );
+      </div>
+      <div className={styles.buttonContainer}>
+        <CustomButton
+          theme="light"
+          text="Back"
+          size={14}
+          className={styles.buttonLeft}
+          onClick={back}
+        />
+      </div>
+    </div>
+  )
 }
 
-export default ViewComponent;
+export default ViewComponent
