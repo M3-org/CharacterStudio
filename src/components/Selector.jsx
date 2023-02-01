01d24b328f93fc0120af88cd42ba43d2791e42ab
memelotsqui <memelotsqui@gmail.com>
2023-02-01 12:30:21 -0600

    copmment out old code

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -445,18 +445,18 @@ export default function Selector({templateInfo, animationManager, blinkManager, 
           createFaceNormals(child.geometry)
           if (child.isSkinnedMesh) createBoneDirection(child)
         }
-        if (child.isBone && child.name == 'neck') { 
-          setTraitsNecks(current => [...current , child])
-        }
-        if (child.isBone && child.name == 'spine') { 
-          setTraitsSpines(current => [...current , child])
-        }
-        if (child.isBone && child.name === 'leftEye') { 
-          setTraitsLeftEye(current => [...current , child])
-        }
-        if (child.isBone && child.name === 'rightEye') { 
-          setTraitsRightEye(current => [...current , child])
-        }
+        // if (child.isBone && child.name == 'neck') { 
+        //   setTraitsNecks(current => [...current , child])
+        // }
+        // if (child.isBone && child.name == 'spine') { 
+        //   setTraitsSpines(current => [...current , child])
+        // }
+        // if (child.isBone && child.name === 'leftEye') { 
+        //   setTraitsLeftEye(current => [...current , child])
+        // }
+        // if (child.isBone && child.name === 'rightEye') { 
+        //   setTraitsRightEye(current => [...current , child])
+        // }
       })
 
       // culling layers setup section
