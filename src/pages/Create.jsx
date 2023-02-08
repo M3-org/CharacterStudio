624c87740f9b190b40c8a8da77235d21ab352551
Jacob LeCoq <lecoqjacob@gmail.com>
2023-02-07 20:15:51 -0600

    flexbox

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -3,7 +3,7 @@ import styles from "./Create.module.css"
 import { ViewMode, ViewContext } from "../context/ViewContext"
 import CustomButton from "../components/custom-button"
 
-function Create({fetchNewModel}) {
+function Create({ fetchNewModel }) {
   const { setViewMode } = React.useContext(ViewContext)
 
   const back = () => {
@@ -11,69 +11,68 @@ function Create({fetchNewModel}) {
   }
 
   const selectClass = (characterClass) => {
-    fetchNewModel(characterClass.templateIndex).then(()=>{
-        setViewMode(ViewMode.APPEARANCE)
+    fetchNewModel(characterClass.templateIndex).then(() => {
+      setViewMode(ViewMode.APPEARANCE)
     })
-
   }
 
   const classes = [
     {
-        name: "Beast Painter",
-        image: "/assets/media/disabled.png",
-        description: "Paints beasts",
-        icon: "/assets/icons/class-beast-painter.svg",
-        disabled: true,
-        templateIndex:2
-      },
-      {
-        name: "Engineer",
-        image: "/assets/media/disabled.png",
-        description: "Builds things",
-        icon: "/assets/icons/class-engineer.svg",
-        disabled: true,
-        templateIndex:3
-      },
-      {
-        name: "Drop Hunter",
-        image: "/assets/media/DropHunter.png",
-        description: "Hunts drops",
-        icon: "/assets/icons/class-drop-hunter.svg",
-        disabled: false,
-        templateIndex:0
-      },
-      {
-        name: "The Degen",
-        image: "/assets/media/degens.gif",
-        description: "Degens",
-        icon: "/assets/icons/class-the-degen.svg",
-        disabled: true,
-        templateIndex:6
-      },
-      {
-        name: "Neural Hacker",
-        image: "/assets/media/NeuralHacker.png",
-        description: "Hacks neural networks",
-        icon: "/assets/icons/class-neural-hacker.svg",
-        disabled: false,
-        templateIndex:1
-      },
-      {
-        name: "Lisk Witch",
-        image: "/assets/media/disabled.png",
-        description: "Witches lisk",
-        icon: "/assets/icons/class-lisk-witch.svg",
-        disabled: true,
-        templateIndex:4
-      },
-      {
-        name: "Bruiser",
-        image: "/assets/media/disabled.png",
-        description: "Bruises things",
-        icon: "/assets/icons/class-bruiser.svg",
-        disabled: true,
-        templateIndex:5
-      },
+      name: "Beast Painter",
+      image: "/assets/media/disabled.png",
+      description: "Paints beasts",
+      icon: "/assets/icons/class-beast-painter.svg",
+      disabled: true,
+      templateIndex: 2,
+    },
+    {
+      name: "Engineer",
+      image: "/assets/media/disabled.png",
+      description: "Builds things",
+      icon: "/assets/icons/class-engineer.svg",
+      disabled: true,
+      templateIndex: 3,
+    },
+    {
+      name: "Drop Hunter",
+      image: "/assets/media/DropHunter.png",
+      description: "Hunts drops",
+      icon: "/assets/icons/class-drop-hunter.svg",
+      disabled: false,
+      templateIndex: 0,
+    },
+    {
+      name: "The Degen",
+      image: "/assets/media/degens.gif",
+      description: "Degens",
+      icon: "/assets/icons/class-the-degen.svg",
+      disabled: true,
+      templateIndex: 6,
+    },
+    {
+      name: "Neural Hacker",
+      image: "/assets/media/NeuralHacker.png",
+      description: "Hacks neural networks",
+      icon: "/assets/icons/class-neural-hacker.svg",
+      disabled: false,
+      templateIndex: 1,
+    },
+    {
+      name: "Lisk Witch",
+      image: "/assets/media/disabled.png",
+      description: "Witches lisk",
+      icon: "/assets/icons/class-lisk-witch.svg",
+      disabled: true,
+      templateIndex: 4,
+    },
+    {
+      name: "Bruiser",
+      image: "/assets/media/disabled.png",
+      description: "Bruises things",
+      icon: "/assets/icons/class-bruiser.svg",
+      disabled: true,
+      templateIndex: 5,
+    },
   ]
 
   return (
@@ -98,7 +97,10 @@ function Create({fetchNewModel}) {
               }
             >
               <div className={styles.classFrame}>
-                <img src={"/assets/backgrounds/class-frame.svg"} className={styles.frame} />
+                <img
+                  src={"/assets/backgrounds/class-frame.svg"}
+                  className={styles.frame}
+                />
                 <div className={styles.mask}>
                   <img src={characterClass["image"]} className={styles.image} />
                 </div>
@@ -109,12 +111,14 @@ function Create({fetchNewModel}) {
                   />
                 )}
               </div>
+
               <div className={styles.icon}>
                 <img
                   src={characterClass["icon"]}
                   alt={characterClass["name"]}
                 />
               </div>
+
               <div className={styles.name}>{characterClass["name"]}</div>
               <div className={styles.description}>
                 {characterClass["description"]}
