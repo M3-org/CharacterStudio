d5549c9ca46365c554bbf9d74460728a1a4d988a
Jacob LeCoq <lecoqjacob@gmail.com>
2023-02-09 17:55:57 -0600

    dynamic loading

--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,63 @@ import React, { Fragment, useContext, useEffect, useState } from "react"
 import * as THREE from "three"
 
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+
+import { SceneContext } from "./context/SceneContext"
 import { ViewMode, ViewContext } from "./context/ViewContext"
 
-import { AnimationManager } from "./library/animationManager"
-import { LookAtManager } from "./library/lookatManager"
-import { BlinkManager } from "./library/blinkManager"
 import { getAsArray } from "./library/utils"
-import Background from "./components/Background"
-import Scene from "./components/Scene"
+import { BlinkManager } from "./library/blinkManager"
+import { LookAtManager } from "./library/lookatManager"
 import { EffectManager } from "./library/effectManager"
+import { AnimationManager } from "./library/animationManager"
 
-import Landing from "./pages/Landing"
-import View from "./pages/View"
-import BioPage from "./pages/Bio"
-import Save from "./pages/Save"
-import Appearance from "./pages/Appearance"
-import Create from "./pages/Create"
-import Load from "./pages/Load"
-import { SceneContext } from "./context/SceneContext"
+const Scene = React.lazy(() => import("./components/Scene"))
+const Background = React.lazy(() => import("./components/Background"))
+
+const View = React.lazy(() => import("./pages/View"))
+const Save = React.lazy(() => import("./pages/Save"))
+const Load = React.lazy(() => import("./pages/Load"))
+const BioPage = React.lazy(() => import("./pages/Bio"))
+const Create = React.lazy(() => import("./pages/Create"))
+const Landing = React.lazy(() => import("./pages/Landing"))
+const Appearance = React.lazy(() => import("./pages/Appearance"))
 
 // dynamically import the manifest
 const assetImportPath = import.meta.env.VITE_ASSET_PATH + "/manifest.json"
-const peresonalityImportPath = import.meta.env.VITE_ASSET_PATH + "/personality.json"
+const peresonalityImportPath =
+  import.meta.env.VITE_ASSET_PATH + "/personality.json"
 
-let cameraDistance;
-const centerCameraTarget = new THREE.Vector3();
-const centerCameraPosition = new THREE.Vector3();
-let centerCameraPositionLength;
-let ndcBias;
+let cameraDistance
+const centerCameraTarget = new THREE.Vector3()
+const centerCameraPosition = new THREE.Vector3()
+let centerCameraPositionLength
+let ndcBias
 
-const cameraDistanceChat = 1.4;
-const centerCameraTargetChat = new THREE.Vector3(0, 1.25, 0);
-const centerCameraPositionChat = new THREE.Vector3(-0.9786403788721187, 1.4036900759197288, 0.9892635490125085); // note: get from `moveCamera({ targetY: 1.25, distance: 1.4 })`
-const centerCameraPositionLengthChat = centerCameraPositionChat.length();
-const ndcBiasChat = 0.35;
+const cameraDistanceChat = 1.4
+const centerCameraTargetChat = new THREE.Vector3(0, 1.25, 0)
+const centerCameraPositionChat = new THREE.Vector3(
+  -0.9786403788721187,
+  1.4036900759197288,
+  0.9892635490125085,
+) // note: get from `moveCamera({ targetY: 1.25, distance: 1.4 })`
+const centerCameraPositionLengthChat = centerCameraPositionChat.length()
+const ndcBiasChat = 0.35
 
-const cameraDistanceOther = 3.2;
-const centerCameraTargetOther = new THREE.Vector3(0, 0.8, 0);
-const centerCameraPositionOther = new THREE.Vector3(-2.2367993753934425, 1.1512971720174363, 2.2612065299409223); // note: get from `moveCamera({ targetY: 0.8, distance: 3.2 })`
-const centerCameraPositionLengthOther = centerCameraPositionOther.length();
-const ndcBiasOther = 0.5;
+const cameraDistanceOther = 3.2
+const centerCameraTargetOther = new THREE.Vector3(0, 0.8, 0)
+const centerCameraPositionOther = new THREE.Vector3(
+  -2.2367993753934425,
+  1.1512971720174363,
+  2.2612065299409223,
+) // note: get from `moveCamera({ targetY: 0.8, distance: 3.2 })`
+const centerCameraPositionLengthOther = centerCameraPositionOther.length()
+const ndcBiasOther = 0.5
 
-const localVector3 = new THREE.Vector3();
-const localVector4 = new THREE.Vector4();
-const localVector4_2 = new THREE.Vector4();
-const xAxis = new THREE.Vector3(1, 0, 0);
-const yAxis = new THREE.Vector3(0, 1, 0);
+const localVector3 = new THREE.Vector3()
+const localVector4 = new THREE.Vector4()
+const localVector4_2 = new THREE.Vector4()
+const xAxis = new THREE.Vector3(1, 0, 0)
+const yAxis = new THREE.Vector3(0, 1, 0)
 
 async function fetchManifest() {
   const manifest = localStorage.getItem("manifest")
@@ -147,15 +158,144 @@ export default function App() {
     effectManager,
   } = resource.read()
 
-  const { viewMode } = useContext(ViewContext)
-
   const [hideUi, setHideUi] = useState(false)
-
   const [animationManager, setAnimationManager] = useState({})
 
-  const { camera, controls, scene, resetAvatar, setAwaitDisplay, setTemplateInfo, templateInfo, moveCamera, setManifest, manifest } = useContext(SceneContext)
+  const {
+    camera,
+    controls,
+    scene,
+    resetAvatar,
+    setAwaitDisplay,
+    setTemplateInfo,
+    templateInfo,
+    moveCamera,
+    setManifest,
+    manifest,
+  } = useContext(SceneContext)
+  const { viewMode } = useContext(ViewContext)
+
   effectManager.camera = camera
   effectManager.scene = scene
+
+  const updateCameraPosition = () => {
+    if (!effectManager.camera) return
+
+    if ([ViewMode.BIO, ViewMode.MINT, ViewMode.CHAT].includes(viewMode)) {
+      // auto move camera
+      if (viewMode === ViewMode.CHAT) {
+        cameraDistance = cameraDistanceChat
+        centerCameraTarget.copy(centerCameraTargetChat)
+        centerCameraPosition.copy(centerCameraPositionChat)
+        centerCameraPositionLength = centerCameraPositionLengthChat
+        ndcBias = ndcBiasChat
+      } else {
+        cameraDistance = cameraDistanceOther
+        centerCameraTarget.copy(centerCameraTargetOther)
+        centerCameraPosition.copy(centerCameraPositionOther)
+        centerCameraPositionLength = centerCameraPositionLengthOther
+        ndcBias = ndcBiasOther
+      }
+
+      localVector4
+        .set(0, 0, centerCameraPositionLength, 1)
+        .applyMatrix4(effectManager.camera.projectionMatrix)
+      localVector4.x /= localVector4.w
+      localVector4.y /= localVector4.w
+      localVector4.z /= localVector4.w
+      const moveX = localVector4_2
+        .set(
+          ndcBias * localVector4.w,
+          localVector4.y * localVector4.w,
+          localVector4.z * localVector4.w,
+          localVector4.w,
+        )
+        .applyMatrix4(effectManager.camera.projectionMatrixInverse).x
+
+      const angle = localVector3
+        .set(centerCameraPosition.x, 0, centerCameraPosition.z)
+        .angleTo(xAxis)
+      localVector3.set(moveX, 0, 0).applyAxisAngle(yAxis, angle)
+      localVector3.add(centerCameraTarget)
+
+      moveCamera({
+        // left half center
+        targetX: localVector3.x,
+        targetY: localVector3.y,
+        targetZ: localVector3.z,
+        distance: cameraDistance,
+      })
+    } else {
+      moveCamera({
+        // center
+        targetX: 0,
+        targetY: centerCameraTargetOther.y,
+        targetZ: 0,
+        distance: cameraDistanceOther,
+      })
+    }
+
+    if (controls) {
+      if (
+        [ViewMode.APPEARANCE, ViewMode.SAVE, ViewMode.MINT].includes(viewMode)
+      ) {
+        controls.enabled = true
+      } else {
+        controls.enabled = false
+      }
+    }
+  }
+
+  const fetchNewModel = (index) => {
+    //setManifest(manifest)
+    setAwaitDisplay(true)
+    resetAvatar()
+    return new Promise((resolve) => {
+      asyncResolve()
+      async function asyncResolve() {
+        const animManager = await fetchAnimation(manifest[index])
+        setAnimationManager(animManager)
+        let initialTraits = localStorage.getItem("initialTraits")
+        if (!initialTraits) {
+          initialTraits = initialTraits = [
+            ...new Set([
+              ...getAsArray(manifest[index].requiredTraits),
+              ...getAsArray(manifest[index].randomTraits),
+            ]),
+          ]
+          localStorage.setItem("initialTraits", JSON.stringify(initialTraits))
+        } else {
+          initialTraits = JSON.parse(initialTraits)
+        }
+
+        setTemplateInfo(manifest[index])
+
+        resolve(manifest[index])
+      }
+    })
+  }
+
+  // map current app mode to a page
+  const pages = {
+    [ViewMode.LANDING]: <Landing />,
+    [ViewMode.APPEARANCE]: (
+      <Appearance
+        animationManager={animationManager}
+        blinkManager={blinkManager}
+        lookatManager={lookatManager}
+        effectManager={effectManager}
+        fetchNewModel={fetchNewModel}
+      />
+    ),
+    [ViewMode.BIO]: (
+      <BioPage templateInfo={templateInfo} personality={personality} />
+    ),
+    [ViewMode.CREATE]: <Create fetchNewModel={fetchNewModel} />,
+    [ViewMode.LOAD]: <Load />,
+    // [ViewMode.MINT]: <Mint />,
+    [ViewMode.SAVE]: <Save />,
+    [ViewMode.CHAT]: <View templateInfo={templateInfo} />,
+  }
 
   let lastTap = 0
   useEffect(() => {
@@ -176,134 +316,32 @@ export default function App() {
     }
   }, [hideUi])
 
-  useEffect(()=>{
-    setManifest(initialManifest)
-  },[initialManifest])
-
-  const updateCameraPosition = () => {
-    if (!effectManager.camera) return;
-
-    if ([ViewMode.BIO, ViewMode.MINT, ViewMode.CHAT].includes(viewMode)) { // auto move camera
-      if (viewMode === ViewMode.CHAT) {
-        cameraDistance = cameraDistanceChat;
-        centerCameraTarget.copy(centerCameraTargetChat);
-        centerCameraPosition.copy(centerCameraPositionChat);
-        centerCameraPositionLength = centerCameraPositionLengthChat;
-        ndcBias = ndcBiasChat;
-      } else {
-        cameraDistance = cameraDistanceOther;
-        centerCameraTarget.copy(centerCameraTargetOther);
-        centerCameraPosition.copy(centerCameraPositionOther);
-        centerCameraPositionLength = centerCameraPositionLengthOther;
-        ndcBias = ndcBiasOther;
-      }
-
-      localVector4.set(0, 0, centerCameraPositionLength ,1).applyMatrix4(effectManager.camera.projectionMatrix);
-      localVector4.x /= localVector4.w;
-      localVector4.y /= localVector4.w;
-      localVector4.z /= localVector4.w;
-      const moveX = localVector4_2.set(ndcBias * localVector4.w, localVector4.y * localVector4.w, localVector4.z * localVector4.w, localVector4.w).applyMatrix4(effectManager.camera.projectionMatrixInverse).x;
-  
-      const angle = localVector3.set(centerCameraPosition.x, 0, centerCameraPosition.z).angleTo(xAxis)
-      localVector3.set(moveX, 0, 0).applyAxisAngle(yAxis, angle);
-      localVector3.add(centerCameraTarget);
-
-      moveCamera({ // left half center
-        targetX: localVector3.x,
-        targetY: localVector3.y,
-        targetZ: localVector3.z,
-        distance: cameraDistance,
-      })
-    } else {
-      moveCamera({ // center
-        targetX: 0,
-        targetY: centerCameraTargetOther.y,
-        targetZ: 0,
-        distance: cameraDistanceOther
-      })
-    }
-
-    if (!controls) return;
-    if ([ViewMode.APPEARANCE, ViewMode.SAVE, ViewMode.MINT].includes(viewMode)) {
-      controls.enabled = true;
-    } else {
-      controls.enabled = false;
-    }
-  }
-  
   useEffect(() => {
-    updateCameraPosition();
-    if ([ViewMode.BIO, ViewMode.MINT, ViewMode.CHAT].includes(viewMode)){
-      lookatManager.enabled = false;
+    updateCameraPosition()
+    if ([ViewMode.BIO, ViewMode.MINT, ViewMode.CHAT].includes(viewMode)) {
+      lookatManager.enabled = false
+    } else {
+      lookatManager.enabled = true
     }
-    else{
-      lookatManager.enabled = true;
-    }
-    window.addEventListener('resize', updateCameraPosition);
+    window.addEventListener("resize", updateCameraPosition)
     return () => {
-      window.removeEventListener('resize', updateCameraPosition);
+      window.removeEventListener("resize", updateCameraPosition)
     }
   }, [viewMode])
 
-  const fetchNewModel = (index) => {
-    //setManifest(manifest)
-    setAwaitDisplay(true)
-    resetAvatar();
-    return new Promise((resolve) => {
-      asyncResolve()
-      async function asyncResolve() {
-       
-        const animManager = await fetchAnimation(manifest[index])
-        setAnimationManager(animManager)
-        let initialTraits = localStorage.getItem("initialTraits")
-        if (!initialTraits) {
-          initialTraits = initialTraits = [
-            ...new Set([
-              ...getAsArray(manifest[index].requiredTraits),
-              ...getAsArray(manifest[index].randomTraits),
-            ]),
-          ]
-          localStorage.setItem("initialTraits", JSON.stringify(initialTraits))
-        } else {
-          initialTraits = JSON.parse(initialTraits)
-        }
+  useEffect(() => {
+    setManifest(initialManifest)
+  }, [initialManifest])
 
-        setTemplateInfo(manifest[index])
-        
-        resolve(manifest[index])
-
-      }
-    })
-  }
-
-  // map current app mode to a page
-  const pages = {
-    [ViewMode.LANDING]: <Landing />,
-    [ViewMode.APPEARANCE]: (
-      <Appearance
-        animationManager={animationManager}
-        blinkManager={blinkManager}
-        lookatManager={lookatManager}
-        effectManager={effectManager}
-        fetchNewModel={fetchNewModel}
-      />
-    ),
-    [ViewMode.BIO]: <BioPage templateInfo={templateInfo} personality={personality} />,
-    [ViewMode.CREATE]: <Create 
-      fetchNewModel={fetchNewModel}
-      />,
-    [ViewMode.LOAD]: <Load />,
-    // [ViewMode.MINT]: <Mint />,
-    [ViewMode.SAVE]: <Save />,
-    [ViewMode.CHAT]: <View templateInfo={templateInfo} />,
-  }
   return (
     <Fragment>
-      <div className="generalTitle">
-        Character Creator
-      </div>
+      <div className="generalTitle">Character Creator</div>
       <Background />
-      <Scene manifest={manifest} sceneModel={sceneModel} lookatManager ={lookatManager}  />
+      <Scene
+        manifest={manifest}
+        sceneModel={sceneModel}
+        lookatManager={lookatManager}
+      />
       {pages[viewMode]}
     </Fragment>
   )
