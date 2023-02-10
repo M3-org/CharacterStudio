1f735f596dad5dc1122f424a856fe7b44ae5ff9c
soulofmischief <30357883+soulofmischief@users.noreply.github.com>
2023-02-10 01:27:06 -0800

    Fix bio storage issues

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -44,12 +44,11 @@ export const getBio = (templateInfo, personality) => {
     personality: q1, //{question, answer}
     relationship: q2,
     hobbies: q3,
-    description, 
+    description,
     greeting:"Hello"
   }
 
-  const bio = `${name} is a ${personality.classes[classType]} from ${city}. ${heshe} is ${hobby}. ${heshe} also enjoys ${profession}. ${heshe} is armed with a ${weapon}.`
-  return { name, bio }
+  return fullBio
 }
 
 export const getPersonalityQuestionsAndAnswers = (personality) => {
@@ -78,6 +77,12 @@ export const getRelationshipQuestionsAndAnswers = (personality) => {
 // Cache voice keys for performance.
 const voiceKeys = Object.keys(voices)
 
+function loadBioFromStorage(itemName){
+  const fullBioStr = localStorage.getItem(itemName)
+  if (fullBioStr)
+    return JSON.parse(fullBioStr)
+  return null
+}
 
 function BioPage({ templateInfo, personality }) {
   const { setViewMode } = React.useContext(ViewContext)
@@ -90,72 +95,16 @@ function BioPage({ templateInfo, personality }) {
     setViewMode(ViewMode.SAVE)
   }
 
-  const _bio = getBio(templateInfo, personality)
-
-  const [name, setName] = React.useState(
-    localStorage.getItem("name")
-    || _bio.name
-  )
-  const [bio, setBio] = React.useState(
-    localStorage.getItem("bio")
-    || _bio.bio
-  )
-  const [voice, setVoice] = React.useState(
-    localStorage.getItem("voice")
-    || voiceKeys[0]
+  const [fullBio, setFullBio] = React.useState(
+    loadBioFromStorage(`${templateInfo.id}_fulBio`)
+    ||
+    getBio(templateInfo, personality)
   )
 
-  const [greeting, setGreeting] = React.useState(
-    localStorage.getItem("greeting") || "Hey there!",
-  )
-
-    const q1 = getPersonalityQuestionsAndAnswers(personality);
-    const q2 = getRelationshipQuestionsAndAnswers(personality);
-    const q3 = getHobbyQuestionsAndAnswers(personality);
-
-  const [question1, setQuestion1] = React.useState(
-    localStorage.getItem("question1") || q1.question,
-  )
-  const [question2, setQuestion2] = React.useState(
-    localStorage.getItem("question2") || q2.question,
-  )
-  const [question3, setQuestion3] = React.useState(
-    localStorage.getItem("question3") || q3.question,
-  )
-  const [response1, setResponse1] = React.useState(
-    localStorage.getItem("response1") || q1.answer,
-  )
-  const [response2, setResponse2] = React.useState(
-    localStorage.getItem("response2") || q2.answer,
-  )
-  const [response3, setResponse3] = React.useState(
-    localStorage.getItem("response3") || q3.answer,
-  )
-
-  // after each state is updated, save to local storage
   React.useEffect(() => {
-    localStorage.setItem("name", name)
-    localStorage.setItem("bio", bio)
-    localStorage.setItem("voice", voice)
-    localStorage.setItem("greeting", greeting)
-    localStorage.setItem("question1", question1)
-    localStorage.setItem("question2", question2)
-    localStorage.setItem("question3", question3)
-    localStorage.setItem("response1", response1)
-    localStorage.setItem("response2", response2)
-    localStorage.setItem("response3", response3)
-  }, [
-    name,
-    bio,
-    voice,
-    greeting,
-    question1,
-    question2,
-    question3,
-    response1,
-    response2,
-    response3,
-  ])
+    localStorage.setItem(`${templateInfo.id}_fulBio`, JSON.stringify(fullBio))
+  }, [fullBio])
+
 
   // if user presses ctrl c, clear the messages
   useEffect(() => {
@@ -178,171 +127,202 @@ function BioPage({ templateInfo, personality }) {
         <div className={styles.bottomLine} />
         <div className={styles.scrollContainer}>
 
-        <div className={styles["inner-container"]}>
-          {/* Name */}
-          <div className={styles.section}>
-            <label
-              className={styles.label}
-              htmlFor="name">
-              Name
-            </label>
+          <div className={styles["inner-container"]}>
+            {/* Name */}
+            <div className={styles.section}>
+              <label
+                className={styles.label}
+                htmlFor="name">
+                Name
+              </label>
 
-            <input
-              type="text"
-              name="name"
-              className={styles.input}
-              defaultValue={name}
-              onChange={(e) => setName(e.target.value)}
-            />
+              <input
+                type="text"
+                name="name"
+                className={styles.input}
+                defaultValue={fullBio.name}
+                onChange={(e) => setFullBio({...fullBio, ...{name:e.target.value}})}
+              />
+            </div>
+
+            {/* Voice */}
+            <div className={styles.section}>
+              <label
+                className={styles.label}
+                htmlFor="voice">
+                Voice
+              </label>
+
+              <select
+                name="voice"
+                className={styles.select}
+                defaultValue={fullBio.voiceKey}
+                onChange={(e) => setFullBio({...fullBio, ...{voiceKey:e.target.value}})}
+              >
+                {voiceKeys.map((option, i) => {
+                  return (
+                    <option key={i} value={option}>
+                      {option}
+                    </option>
+                  )
+                })}
+              </select>
+            </div>
+
+            {/* Preferred Greeting */}
+            <div className={styles.section}>
+              <label
+                className={styles.label}
+                htmlFor="greeting">
+                Preferred Greeting
+              </label>
+
+              <input
+                type="text"
+                name="greeting"
+                className={styles.input}
+                defaultValue={fullBio.greeting}
+                onChange={(e) => setFullBio({...fullBio, ...{greeting:e.target.value}})}
+              />
+            </div>
+
+            {/* Bio */}
+            <div className={styles.section}>
+              <label className={styles.label} htmlFor="bio">Bio</label>
+
+              <textarea
+                name="bio"
+                className={styles.input}
+                rows="4"
+                cols="50"
+                defaultValue={fullBio.description}
+                onChange={(e) => setFullBio({...fullBio, ...{description:e.target.value}})}
+              />
+            </div>
+
+            {/* Question 1 */}
+            <div className={styles.section}>
+              <label
+                className={styles.label}
+                htmlFor="question1">
+                Question 1
+              </label>
+
+              <select
+                name="question1"
+                className={styles.select}
+                defaultValue={fullBio.personality.question}
+                onChange={(e) => setFullBio({...fullBio, ...{
+                    personality:{
+                      question:e.target.value,
+                      answer:fullBio.personality.answer
+                    }
+                  }})}
+              >
+                {personality.generalPersonalityQuestions.map((question, i) => {
+                  return (
+                    <option key={i} value={question}>
+                      {question}
+                    </option>
+                  )
+                })}
+              </select>
+              <textarea
+                name="response1"
+                className={styles.input}
+                defaultValue={fullBio.personality.answer}
+                onChange={(e) => setFullBio({...fullBio, ...{
+                    personality:{
+                      question:fullBio.personality.question,
+                      answer:e.target.value
+                    }
+                  }})}
+
+
+              />
+            </div>
+
+            {/* Question 2 */}
+            <div className={styles.section}>
+              <label
+                className={styles.label}
+                htmlFor="question2">
+                Question 2
+              </label>
+
+              <select
+                name="question2"
+                className={styles.select}
+                defaultValue={fullBio.relationship.question}
+                onChange={(e) => setFullBio({...fullBio, ...{
+                    relationship:{
+                      question:e.target.value,
+                      answer:fullBio.relationship.answer
+                    }
+                  }})}
+              >
+                {personality.relationshipQuestions.map((question, i) => {
+                  return (
+                    <option key={i} value={question}>
+                      {question}
+                    </option>
+                  )
+                })}
+              </select>
+
+              <textarea
+                name="response1"
+                className={styles.input}
+                defaultValue={fullBio.relationship.answer}
+                onChange={(e) => setFullBio({...fullBio, ...{
+                    relationship:{
+                      question:fullBio.relationship.question,
+                      answer:e.target.value
+                    }
+                  }})}
+              />
+            </div>
+
+            {/* Question 3 */}
+            <div className={styles.section}>
+              <label
+                className={styles.label}
+                htmlFor="question3">
+                Question 3
+              </label>
+              <select
+                name="question3"
+                className={styles.select}
+                defaultValue={fullBio.hobbies.question}
+                onChange={(e) => setFullBio({...fullBio, ...{
+                    hobbies:{
+                      question:e.target.value,
+                      answer: fullBio.hobbies.answer
+                    }
+                  }})}
+              >
+                {personality.hobbyQuestions.map((question, i) => {
+                  return (
+                    <option key={i} value={question}>
+                      {question}
+                    </option>
+                  )
+                })}
+              </select>
+
+              <textarea
+                name="response3"
+                className={styles.input}
+                defaultValue={fullBio.hobbies.answer}
+                onChange={(e) => setFullBio({...fullBio, ...{
+                    hobbies:{
+                      question:fullBio.hobbies.question,
+                      answer:e.target.value
+                    }
+                  }})}
+              />
+            </div>
           </div>
-
-          {/* Voice */}
-          <div className={styles.section}>
-            <label
-              className={styles.label}
-              htmlFor="voice">
-              Voice
-            </label>
-
-            <select
-              name="voice"
-              className={styles.select}
-              defaultValue={voice}
-              onChange={(e) => setVoice(e.target.value)}
-            >
-              {voiceKeys.map((option, i) => {
-                return (
-                  <option key={i} value={option}>
-                    {option}
-                  </option>
-                )
-              })}
-            </select>
-          </div>
-
-          {/* Preferred Greeting */}
-          <div className={styles.section}>
-            <label
-              className={styles.label}
-              htmlFor="greeting">
-              Preferred Greeting
-            </label>
-
-            <input
-              type="text"
-              name="greeting"
-              className={styles.input}
-              defaultValue={greeting}
-              onChange={(e) => setGreeting(e.target.value)}
-            />
-          </div>
-
-          {/* Bio */}
-          <div className={styles.section}>
-            <label className={styles.label} htmlFor="bio">Bio</label>
-
-            <textarea
-              name="bio"
-              className={styles.input}
-              rows="4"
-              cols="50"
-              defaultValue={bio}
-              onChange={(e) => setBio(e.target.value)}
-            />
-          </div>
-
-          {/* Question 1 */}
-          <div className={styles.section}>
-            <label
-              className={styles.label}
-              htmlFor="question1">
-              Question 1
-            </label>
-
-            <select
-              name="question1"
-              className={styles.select}
-              defaultValue={question1}
-              onChange={(e) => setQuestion1(e.target.value)}
-            >
-              {personality.generalPersonalityQuestions.map((question, i) => {
-                return (
-                  <option key={i} value={question}>
-                    {question}
-                  </option>
-                )
-              })}
-            </select>
-            <textarea
-              name="response1"
-              className={styles.input}
-              onChange={(e) => setResponse1(e.target.value)}
-              defaultValue={response1}
-            />
-          </div>
-
-          {/* Question 2 */}
-          <div className={styles.section}>
-            <label
-              className={styles.label}
-              htmlFor="question2">
-              Question 2
-            </label>
-
-            <select
-              name="question2"
-              className={styles.select}
-              defaultValue={question2}
-              onChange={(e) => setQuestion2(e.target.value)}
-            >
-              {personality.relationshipQuestions.map((question, i) => {
-                return (
-                  <option key={i} value={question}>
-                    {question}
-                  </option>
-                )
-              })}
-            </select>
-
-            <textarea
-              name="response1"
-              className={styles.input}
-              defaultValue={response2}
-              onChange={(e) => setResponse2(e.target.value)}
-            />
-          </div>
-
-          {/* Question 3 */}
-          <div className={styles.section}>
-            <label
-              className={styles.label}
-              htmlFor="question3">
-              Question 3
-            </label>
-
-            <select
-              name="question3"
-              className={styles.select}
-              defaultValue={question3}
-              onChange={(e) => setQuestion3(e.target.value)}
-            >
-              {personality.hobbyQuestions.map((question, i) => {
-                return (
-                  <option key={i} value={question}>
-                    {question}
-                  </option>
-                )
-              })}
-            </select>
-
-            <textarea
-              name="response3"
-              className={styles.input}
-              defaultValue={response3}
-              onChange={(e) => setResponse3(e.target.value)}
-            />
-          </div>
-        </div>
         </div>
       </div>
       <div className={styles.buttonContainer}>
