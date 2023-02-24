2e43c7f70acf2c076b681d913b55e66bbab5a3c6
Hayk <55106546+mavisakalyan@users.noreply.github.com>
2023-02-25 01:17:56 +0800

    Move time next to name

--- src/components/message/index.jsx
+++ src/components/message/index.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react"
-import classnames from "classnames"
 import styles from "./Message.module.css"
-import { timeSince } from "../../lib/helpers/time"
 
 export const Message = (props) => {
   const { timestamp, name, message, type, color } = props
   let date = new Date(timestamp)
-  const time = `${date.getHours()}:${date.getMinutes()}`
+  const hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+  const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  const time = `${hours}:${minutes}`
   return (
     <div className={`${styles["message"]} ${type ? styles["speaker"] : styles["agent"]}`}>
       <div className={styles["name"]} style={!type ? { color: color } : {}}>
-        {name}
+        {name}, <span>{time}</span>
       </div>
       <div className={styles["chat-bubble"]}>
         <div
@@ -18,7 +18,6 @@ export const Message = (props) => {
           style={!type ? { backgroundColor: color } : {}}
         />
         {message}
-        <span>{time}</span>
       </div>
     </div>
   )
