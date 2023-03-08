0c220a2d4e2da1405589e249ada0014b6ca2b52a
memelotsqui <memelotsqui@gmail.com>
2023-03-08 10:56:54 -0600

    final merge conflicts

--- src/components/ExportMenu.jsx
+++ src/components/ExportMenu.jsx
@@ -1,7 +1,4 @@
-import { useWeb3React } from "@web3-react/core"
-import { ethers } from "ethers"
-import React, { useContext, useEffect, useState } from "react"
-import { AccountContext } from "../context/AccountContext"
+import React, { useContext } from "react"
 import { SceneContext } from "../context/SceneContext"
 import CustomButton from "./custom-button"
 
@@ -13,57 +10,11 @@ import { LanguageContext } from "../context/LanguageContext"
 const defaultName = "Anon"
 
 export const ExportMenu = () => {
-  // const type = "_Gen1" // class type
-
   // Translate hook
   const { t } = useContext(LanguageContext);
 
-  const [showDownloadOptions, setShowDownloadOptions] = useState(false)
-  const { ensName, setEnsName, connected, setConnected } =
-    useContext(AccountContext)
-  const { activate, deactivate, account } = useWeb3React()
-
-  const [name] = React.useState(
-    localStorage.getItem("name")
-    || defaultName
-  )
-
-  const injected = new InjectedConnector({
-    supportedChainIds: [137, 1, 3, 4, 5, 42, 97],
-  })
-
   const [name] = React.useState(localStorage.getItem("name") || defaultName)
   const { model, avatar } = useContext(SceneContext)
-
-  useEffect(() => {
-    if (account) {
-      _setAddress(account)
-      setConnected(true)
-    } else {
-      setConnected(false)
-    }
-  }, [account])
-
-  const _setAddress = async (address) => {
-    const { name } = await getAccountDetails(address)
-    setEnsName(name ? name.slice(0, 15) + "..." : "")
-  }
-
-  const getAccountDetails = async (address) => {
-    const provider = ethers.getDefaultProvider("mainnet", {
-      alchemy: import.meta.env.VITE_ALCHEMY_API_KEY,
-    })
-    const check = ethers.utils.getAddress(address)
-
-    try {
-      const name = await provider.lookupAddress(check)
-      if (!name) return {}
-      return { name }
-    } catch (err) {
-      console.warn(err.stack)
-      return {}
-    }
-  }
 
   return (
     <React.Fragment>
