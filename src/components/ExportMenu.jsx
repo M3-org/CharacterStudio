46b7fb4b3bda4ea57b38d68df60d694cc75ca2c6
memelotsqui <memelotsqui@gmail.com>
2023-11-14 00:35:47 -0600

    update export menu to send options as parameters

--- src/components/ExportMenu.jsx
+++ src/components/ExportMenu.jsx
@@ -3,6 +3,7 @@ import { SceneContext } from "../context/SceneContext"
 import CustomButton from "./custom-button"
 
 import { downloadGLB, downloadVRMWithAvatar } from "../library/download-utils"
+import { getAtlasSize } from "../library/utils"
 
 import styles from "./ExportMenu.module.css"
 import { local } from "../library/store"
@@ -15,6 +16,32 @@ export const ExportMenu = ({getFaceScreenshot}) => {
   const { t } = useContext(LanguageContext);
   const [name] = React.useState(localStorage.getItem("name") || defaultName)
   const { model, avatar,templateInfo } = useContext(SceneContext)
+
+  const getOptions = () =>{
+    const currentOption = local["mergeOptions_sel_option"] || 0;
+    const screenshot = getFaceScreenshot();
+    console.log(local["mergeOptions_create_atlas"]);
+    const createTextureAtlas = local["mergeOptions_create_atlas"] == null ? true:local["mergeOptions_create_atlas"] 
+    return {
+      isVrm0 : true,
+      createTextureAtlas : createTextureAtlas,
+      mToonAtlasSize:getAtlasSize(local["mergeOptions_atlas_mtoon_size"] || 6),
+      mToonAtlasSizeTransp:getAtlasSize(local["mergeOptions_atlas_mtoon_transp_size"] || 6),
+      stdAtlasSize:getAtlasSize(local["mergeOptions_atlas_std_size"] || 6),
+      stdAtlasSizeTransp:getAtlasSize(local["mergeOptions_atlas_std_transp_size"] || 6),
+      exportStdAtlas:(currentOption === 0 || currentOption == 2),
+      exportMtoonAtlas:(currentOption === 1 || currentOption == 2),
+      screenshot:screenshot,
+      scale:templateInfo.exportScale||1,
+      vrmMeta:templateInfo.vrmMeta
+    }
+  }
+
+  const downloadModel = () =>{
+    const options = getOptions();
+    console.log(options)
+    downloadVRMWithAvatar(model, avatar, name, options)
+  }
 
   return (
     <React.Fragment>
@@ -40,27 +67,11 @@ export const ExportMenu = ({getFaceScreenshot}) => {
       />
       <CustomButton
         theme="light"
-        text="VRM (No Atlas)"
+        text="VRM"
         icon="download"
         size={14}
         className={styles.button}
-        onClick={() => {
-          const screenshot = getFaceScreenshot();
-          const options = {screenshot:screenshot, atlasSize: 4096, scale:templateInfo.exportScale||1, isVrm0:true, vrmMeta:templateInfo.vrmMeta,createTextureAtlas:false}
-          downloadVRMWithAvatar(model, avatar, name, options)
-        }}
-      />
-      <CustomButton
-        theme="light"
-        text="VRM (Atlas)"
-        icon="download"
-        size={14}
-        className={styles.button}
-        onClick={() => {
-          const screenshot = getFaceScreenshot();
-          const options = {screenshot:screenshot, atlasSize: 4096, scale:templateInfo.exportScale||1, isVrm0:true, vrmMeta:templateInfo.vrmMeta,createTextureAtlas:true}
-          downloadVRMWithAvatar(model, avatar, name, options)
-        }}
+        onClick={downloadModel}
       />
     </React.Fragment>
   )
