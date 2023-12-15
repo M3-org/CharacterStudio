a4ffbfb0fc5327b6bc2fe2c3eacfcfd59156a75e
madjin <32600939+madjin@users.noreply.github.com>
2023-12-15 01:54:57 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CharacterStudio/', // Add this line
+  //base: '/CharacterStudio/', // Add this line
   plugins: [react()],
 });
 
