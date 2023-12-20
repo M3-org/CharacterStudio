3424f891df922d2389abaa769d26e166e04489e8
memelotsqui <memelotsqui@gmail.com>
2023-12-19 23:16:49 -0600

    cleanup character manifest data

--- src/library/CharacterManifestData.js
+++ src/library/CharacterManifestData.js
@@ -529,3 +529,138 @@ class SelectedOption{
     this.traitColor = traitColor;
   }
 }
+
+
+
+//  const getRestrictions = () => {
+
+//     const traitRestrictions = templateInfo.traitRestrictions // can be null
+//     const typeRestrictions = {};
+
+//     for (const prop in traitRestrictions){
+
+//       // create the counter restrcitions traits
+//       getAsArray(traitRestrictions[prop].restrictedTraits).map((traitName)=>{
+
+//         // check if the trait restrictions exists for the other trait, if not add it
+//         if (traitRestrictions[traitName] == null) traitRestrictions[traitName] = {}
+//         // make sure to have an array setup, if there is none, create a new empty one
+//         if (traitRestrictions[traitName].restrictedTraits == null) traitRestrictions[traitName].restrictedTraits = []
+
+//         // finally merge existing and new restrictions
+//         traitRestrictions[traitName].restrictedTraits = [...new Set([
+//           ...traitRestrictions[traitName].restrictedTraits ,
+//           ...[prop]])]  // make sure to add prop as restriction
+//       })
+
+//       // do the same for the types
+//       getAsArray(traitRestrictions[prop].restrictedTypes).map((typeName)=>{
+//         //notice were adding the new data to typeRestrictions and not trait
+//         if (typeRestrictions[typeName] == null) typeRestrictions[typeName] = {}
+//         //create the restricted trait in this type
+//         if (typeRestrictions[typeName].restrictedTraits == null) typeRestrictions[typeName].restrictedTraits = []
+
+//         typeRestrictions[typeName].restrictedTraits = [...new Set([
+//           ...typeRestrictions[typeName].restrictedTraits ,
+//           ...[prop]])]  // make sure to add prop as restriction
+//       })
+//     }
+
+//     // now merge defined type to type restrictions
+//     for (const prop in templateInfo.typeRestrictions){
+//       // check if it already exsits
+//       if (typeRestrictions[prop] == null) typeRestrictions[prop] = {}
+//       if (typeRestrictions[prop].restrictedTypes == null) typeRestrictions[prop].restrictedTypes = []
+//       typeRestrictions[prop].restrictedTypes = [...new Set([
+//         ...typeRestrictions[prop].restrictedTypes ,
+//         ...getAsArray(templateInfo.typeRestrictions[prop])])]  
+
+//       // now that we have setup the type restrictions, lets counter create for the other traits
+//       getAsArray(templateInfo.typeRestrictions[prop]).map((typeName)=>{
+//         // prop = boots
+//         // typeName = pants
+//         if (typeRestrictions[typeName] == null) typeRestrictions[typeName] = {}
+//         if (typeRestrictions[typeName].restrictedTypes == null) typeRestrictions[typeName].restrictedTypes =[]
+//         typeRestrictions[typeName].restrictedTypes = [...new Set([
+//           ...typeRestrictions[typeName].restrictedTypes ,
+//           ...[prop]])]  // make sure to add prop as restriction
+//       })
+//     }
+
+    // _filterRestrictedOptions(options){
+    //     let removeTraits = [];
+    //     for (let i =0; i < options.length;i++){
+    //       const option = options[i];
+          
+    //      //if this option is not already in the remove traits list then:
+    //      if (!removeTraits.includes(option.trait.name)){
+    //         const typeRestrictions = restrictions?.typeRestrictions;
+    //         // type restrictions = what `type` cannot go wit this trait or this type
+    //         if (typeRestrictions){
+    //           getAsArray(option.item?.type).map((t)=>{
+    //             //combine to array
+    //             removeTraits = [...new Set([
+    //               ...removeTraits , // get previous remove traits
+    //               ...findTraitsWithTypes(getAsArray(typeRestrictions[t]?.restrictedTypes)),  //get by restricted traits by types coincidence
+    //               ...getAsArray(typeRestrictions[t]?.restrictedTraits)])]  // get by restricted trait setup
+    
+    //           })
+    //         }
+    
+    //         // trait restrictions = what `trait` cannot go wit this trait or this type
+    //         const traitRestrictions = restrictions?.traitRestrictions;
+    //         if (traitRestrictions){
+    //           removeTraits = [...new Set([
+    //             ...removeTraits,
+    //             ...findTraitsWithTypes(getAsArray(traitRestrictions[option.trait.name]?.restrictedTypes)),
+    //             ...getAsArray(traitRestrictions[option.trait.name]?.restrictedTraits),
+    
+    //           ])]
+    //         }
+    //       }
+    //     }
+    
+    //     // now update uptions
+    //     removeTraits.forEach(trait => {
+    //       let removed = false;
+    //       updateCurrentTraitMap(trait, null);
+          
+    //       for (let i =0; i < options.length;i++){
+    //         // find an option with the trait name 
+    //         if (options[i].trait?.name === trait){
+    //           options[i] = {
+    //             item:null,
+    //             trait:templateInfo.traits.find((t) => t.name === trait)
+    //           }
+    //           removed = true;
+    //           break;
+    //         }
+    //       }
+    //       // if no option setup was found, add a null option to remove in case user had it added before
+    //       if (!removed){
+    //         options.push({
+    //           item:null,
+    //           trait:templateInfo.traits.find((t) => t.name === trait)
+    //         })
+    //       }
+    //     });
+       
+    //     return options;
+    // }
+
+        // const findTraitsWithTypes = (types) => {
+        //   const typeTraits = [];
+        //   for (const prop in avatar){
+        //     for (let i = 0; i < types.length; i++){
+        //       const t = types[i]
+            
+        //       if (avatar[prop].traitInfo?.type?.includes(t)){
+        //         typeTraits.push(prop);
+        //         break;
+        //       }
+        //     }
+        //   }
+        //   return typeTraits;
+        // }
+ 
+    
