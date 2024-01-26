c58411b0a23ab91d3d2f2c9c0f21455244bc9c99
moon <shawmakesmagic@gmail.com>
2024-01-25 16:51:21 -0800

    change base

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CharacterStudio/',
   build: {
     outDir: './build',
-    base: '/CharacterStudio/',
   }
 })
