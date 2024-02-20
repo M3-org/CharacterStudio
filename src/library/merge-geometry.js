9b938fbf2a7c72d7926ebd81f119f492a05dae73
memelotsqui <memelotsqui@gmail.com>
2024-02-19 23:59:42 -0600

    pose character and comment previous clone skeleton function

--- src/library/merge-geometry.js
+++ src/library/merge-geometry.js
@@ -5,13 +5,42 @@ import { findChildrenByType, getMeshesSortedByMaterialArray } from "./utils.js";
 import { createTextureAtlas } from "./create-texture-atlas.js";
 import { BufferAttribute } from "three";
 
+// previous method, it doesnt consider added bones (upper chest), might come back to it when 
+// animations work with vrm animations instead of bone renaming
+// export function cloneSkeleton(skinnedMesh) {
+//     const boneClones = new Map();
+//     for (const bone of skinnedMesh.skeleton.bones) {
+//         const clone = bone.clone(false);
+//         // clone.position.x *= -1;
+//         // clone.position.z *= -1;
+//         boneClones.set(bone, clone);
+//     }
+//     // Preserve original bone structure
+//     // Assume bones[0] is root bone
+//     skinnedMesh.skeleton.bones[0].traverse((o) => {
+//         if (o.type !== "Bone")
+//             return;
+            
+//         const clone = boneClones.get(o);
+//         for (const child of o.children) {
+//             const ch = boneClones.get(child);
+//             if (ch)clone.add(ch);
+//         }
+//     });
+//     const newSkeleton = new THREE.Skeleton(skinnedMesh.skeleton.bones.map((b) => boneClones.get(b)));
+//     newSkeleton.boneInverses = skinnedMesh.skeleton.boneInverses;
+//     newSkeleton.pose();
+//     return newSkeleton;
+// }
+
 export function cloneSkeleton(skinnedMesh) {
     const boneClones = new Map();
+    skinnedMesh.skeleton.pose();
     skinnedMesh.skeleton.bones[0].traverse((o) =>{
         if (o.type === "Bone"){
             const clone = o.clone(false);
             // clone.position.x *= -1;
-            // clone.position.z *= -1;
+            // clone.position.z *= -1; 
             boneClones.set(o, clone);
         }
     });
@@ -50,7 +79,7 @@ export function cloneSkeleton(skinnedMesh) {
         }
     });
     const newSkeleton = new THREE.Skeleton(skeletonBonesUpdated.map((b) => boneClones.get(b)));
-    newSkeleton.boneInverses = skeletonBonesInverses;
+    //newSkeleton.boneInverses = skeletonBonesInverses;
     newSkeleton.pose();
     return newSkeleton;
 }
