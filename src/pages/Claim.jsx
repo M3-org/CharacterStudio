74c0ddadbcdcde9113de679d10e133918c7f1254
memelotsqui <memelotsqui@gmail.com>
2024-04-07 11:38:02 -0600

    update pages to reflect new manifest structure

--- src/pages/Claim.jsx
+++ src/pages/Claim.jsx
@@ -21,8 +21,8 @@ function Claim() {
   const [ classes, setClasses ] = useState([]) 
   
   useEffect(() => {
-    if (manifest != null){
-      const manifestClasses = manifest.map((c) => {
+    if (manifest?.characters != null){
+      const manifestClasses = manifest.characters.map((c) => {
         return {
           name:c.name, 
           image:c.portrait, 
@@ -45,7 +45,7 @@ function Claim() {
   const selectClass = async (index) => {
     setIsLoading(true)
     // Load manifest first
-    characterManager.loadManifest(manifest[index].manifest).then(()=>{
+    characterManager.loadManifest(manifest.characters[index].manifest).then(()=>{
       setViewMode(ViewMode.BATCHDOWNLOAD)
       // When Manifest is Loaded, load initial traits from given manifest
       characterManager.loadInitialTraits().then(()=>{
