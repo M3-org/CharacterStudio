23e0d996fcff1a26313bc312e409ea60d7178be0
M̵̞̗̝̼̅̏̎͝Ȯ̴̝̻̊̃̋̀Õ̷̼͋N̸̩̿͜ ̶̜̠̹̼̩͒ <shawmakesmagic@gmail.com>
2024-10-15 16:50:45 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CharacterStudio/',
+//  base: '/CharacterStudio/', NOTE: For Github pages, you need to add the base URL (name of the repo)
   build: {
     outDir: './build',
   }
